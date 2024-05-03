4ebe5c8d0a3db0e42aafdffe0913f4b7196269b7
Suhail <167235612+suhailsaifi034@users.noreply.github.com>
2024-05-03 11:41:27 +0530

    Add files via upload

---
+++ src/Components/Home/Filter.js
@@ -0,0 +1,52 @@
+import React, {useEffect, useState} from 'react';
+import FilterModal from './FilterModal';
+import { UseDispatch, useDispatch } from 'react-redux';
+import { getAllProperties } from '../../Store/Property/property-action';
+import { propertyAction } from '../../Store/Property/property-slice';
+
+const Filter = () => {
+    //state for controlling modal visability
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    //state for storing selected filters
+    const [selectedFilters, setSelectedFilters] = useState({});
+    const dispatch = useDispatch();
+    useEffect (() => {
+        dispatch(propertyAction.updateSearchParams(selectedFilters));
+        dispatch(getAllProperties());
+    }, [selectedFilters, dispatch]);
+    
+    //function to handle opening the modal/popupWindow
+    const handleOpenModal = () => {
+        setIsModalOpen(true);//sets isModalOpen to true to open the modal
+    };
+
+    //function to handle closing the modal
+    const handleCloseModal = () => {
+        setIsModalOpen(false);//sets isModalOpen to false to close the modal
+    };
+
+    //function to handle changes in filters
+    const handleFilterChange = (filterName, value) => {
+        //Update the selected  filters with the new values
+        setSelectedFilters((prevFilters) => ({
+            ...prevFilters,
+            [filterName]: value,
+        }));
+    };
+  return (
+    <>
+        <span class="material-symbols-outlined filter" onClick={handleOpenModal}>
+            tune
+        </span>
+        {isModalOpen && (
+            <FilterModal
+                selectedFilters={selectedFilters}
+                onFilterChange={handleFilterChange}
+                onClose={handleCloseModal}
+            />
+        )}
+    </>
+  );
+};
+
+export default Filter;
